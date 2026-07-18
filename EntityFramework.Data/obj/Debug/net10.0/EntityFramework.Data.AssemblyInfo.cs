5aef3d1a16ad39b082a69883ca701f0d3f7046a4
bogdantwelves <bohdap@taltech.ee>
2026-07-18 15:25:26 +0300

    Fix author

--- EntityFramework.Data/obj/Debug/net10.0/EntityFramework.Data.AssemblyInfo.cs
+++ EntityFramework.Data/obj/Debug/net10.0/EntityFramework.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityFramework.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc97f84cc14c905b2881c22969c934416c37631")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityFramework.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityFramework.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EntityFramework.Domain/obj/Debug/net10.0/EntityFramework.Domain.AssemblyInfo.cs
+++ EntityFramework.Domain/obj/Debug/net10.0/EntityFramework.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityFramework.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc97f84cc14c905b2881c22969c934416c37631")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityFramework.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityFramework.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

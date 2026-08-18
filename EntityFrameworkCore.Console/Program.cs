933214e5e9332d0e003d07e504bacddd19c67573
bogdantwelves <bohdap@taltech.ee>
2026-08-18 21:44:03 +0300

    practiced additional filtering features

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -4,19 +4,44 @@ using Microsoft.EntityFrameworkCore;
 var context = new FootballLeagueDbContext();
 
 //GetAllTeams();
-//Selecting a single record - first team
-var teamOne = await context.Teams.FirstAsync();
-//Selecting a single record - first one that meet a condition
-var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
-//Selecting based on ID
-var teamBaseOnId = await context.Teams.FindAsync(2);
-Console.WriteLine(teamBaseOnId.Name);
-void GetAllTeams()
+//GetOneTeam();
+//Select all methods that meat a condition
+GetFiltredTeams();
+async Task GetFiltredTeams()
 {
-    var teams = context.Teams.ToList();
+    Console.WriteLine("Write ur team");
+    var searchTerm = Console.ReadLine();
+    var teamsFiltred = await context.Teams.Where(q => q.Name == searchTerm).ToListAsync();
+
+    foreach (var team in teamsFiltred)
+    {
+        Console.WriteLine(team.TeamId);
+    }
+    
+    // var partialMatches = await context.Teams.Where(q => q.Name.Contains(searchTerm)).ToListAsync();
+    var partialMatches = await context.Teams.Where(q => EF.Functions.Like(q.Name, $"%{searchTerm}%")).ToListAsync();
+    foreach (var team in partialMatches)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
+async void GetAllTeams()
+{
+    var teams = await context.Teams.ToListAsync();
     
     foreach (var t in teams)
     {
         Console.WriteLine(t.Name);
     }
+}
+
+async void GetOneTeam()
+{
+    //Selecting a single record - first team
+    var teamOne = await context.Teams.FirstAsync();
+    //Selecting a single record - first one that meet a condition
+    var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
+    //Selecting based on ID
+    var teamBaseOnId = await context.Teams.FindAsync(2);
+    Console.WriteLine(teamBaseOnId.Name);
 }
--- EntityFrameworkCore.Console/obj/Debug/net10.0/EntityFrameworkCore.Console.AssemblyInfo.cs
+++ EntityFrameworkCore.Console/obj/Debug/net10.0/EntityFrameworkCore.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityFrameworkCore.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd705b6ce49b07415f7de96b761bc2dc78edbe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf69f02a1b2963b0061ba6bcec95f2e5a4c8380")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityFrameworkCore.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityFrameworkCore.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
